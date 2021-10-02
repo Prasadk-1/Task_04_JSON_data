b8681cadc99f2e4e591606cdccb46e56668655bc
Prasadk-1 <87906300+Prasadk-1@users.noreply.github.com>
2021-10-02 12:05:42 +0530

    Update script.js
    saved the changes

--- script.js
+++ script.js
@@ -38,6 +38,7 @@ xhr.onload = () => {
 getCountries();
 
 
+//task03
 
 const getflags = () => {
   const xhr = new XMLHttpRequest();
@@ -65,85 +66,3 @@ const UserList = document.querySelector('.flag-display');
 
 
 getflags();
-
-
-
-
-
-
-
-
-// function ProgressCountdown(timeleft, text) {
-//   return new Promise((resolve, reject) => {
-//     var countdownTimer = setInterval(() => {
-//       timeleft--;
-
-     
-//       document.getElementById(text).textContent = timeleft;
-
-//       if (timeleft <= 0) {
-//         clearInterval(countdownTimer);
-//         resolve(true);
-//       }
-      
-//     }, 1000);
-    
-//   });
-// }
-
-// ProgressCountdown(10,  'pageBeginCountdownText').then(value => document.write(`Happy New Year`));
-
-
-
-// var number = 10;
-
-// setTimeout(() => {
-//  var count = document.getElementById("head");
-//  count.innerHtml = number;
-//  setTimeout(() => {
-//   var count = document.getElementById("head");
-//   count.innerText = number--;
-//   setTimeout(() => {
-//    var count = document.getElementById("head");
-//    count.innerText = number--;
-//    setTimeout(() => {
-//     var count = document.getElementById("head");
-//     count.innerText = number--;
-//     setTimeout(() => {
-//      var count = document.getElementById("head");
-//      count.innerText = number--;
-//      setTimeout(() => {
-//       var count = document.getElementById("head");
-//       count.innerText = number--;
-//       setTimeout(() => {
-//        var count = document.getElementById("head");
-//        count.innerText = number--;
-//        setTimeout(() => {
-//         var count = document.getElementById("head");
-//         count.innerText = number--;
-//         setTimeout(() => {
-//          var count = document.getElementById("head");
-//          count.innerText = number--;
-//          setTimeout(() => {
-//           var count = document.getElementById("head");
-//           count.innerText = number--;
-//           setTimeout(() => {
-//             var count = document.getElementById("head");
-//             count.innerText = number--;
-//           setTimeout(() => {
-//            var count = document.getElementById("head");
-//            count.innerText = "Happy Independence Day...!!!";
-//            },1000);
-//           },1000);
-//          },1000);
-//         },1000);
-//        },1000);
-//       },1000);
-//      },1000);
-//     },1000);
-//    },1000);
-//   },1000);
-//  },1000);
-// },1000);
-
-
